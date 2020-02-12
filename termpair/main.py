2102cfd71086f5112bc6d9ac83d0be9e7c3a4d32
Chad Smith <cs01@users.noreply.github.com>
2020-02-11 20:47:52 -0800

    upgrade xterm.js

--- termpair/main.py
+++ termpair/main.py
@@ -9,7 +9,7 @@ from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware  # type: 
 import uvicorn  # type: ignore
 from . import share, server
 
-__version__ = "0.0.1.2"
+__version__ = "0.0.1.3"
 
 
 def main():
