a4d4903cec5eb18688dfd02000b9e9edcf5b6679
Chad Smith <cs01@users.noreply.github.com>
2020-10-17 13:50:16 -0700

    increment version

--- termpair/main.py
+++ termpair/main.py
@@ -9,7 +9,7 @@ from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware  # type: 
 import uvicorn  # type: ignore
 from . import share, server
 
-__version__ = "0.1.0.0b0"
+__version__ = "0.1.0.0"
 
 
 def main():
