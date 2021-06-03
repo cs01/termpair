a0dba255faffa178f1b0f80a3e3b8aa3dd21c2c9
Chad Smith <cs01@users.noreply.github.com>
2021-06-02 22:48:31 -0700

    commit built frontend

---
+++ connect/precache-manifest.634ed83aba9a45637903b59c90cb7eec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e764eb75b9d863da86c",
+    "url": "./static/css/main.9fbae2cc.chunk.css"
+  },
+  {
+    "revision": "6e764eb75b9d863da86c",
+    "url": "./static/js/main.d9b0979b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "f8a633bf7d3d5948329a",
+    "url": "./static/css/2.19f2448e.chunk.css"
+  },
+  {
+    "revision": "f8a633bf7d3d5948329a",
+    "url": "./static/js/2.5b077821.chunk.js"
+  },
+  {
+    "revision": "74cdedb8050c1198d3c477e62d8bd849",
+    "url": "./static/media/logo.74cdedb8.png"
+  },
+  {
+    "revision": "15c4b04427922beffe85590746188e26",
+    "url": "./index.html"
+  }
+];
