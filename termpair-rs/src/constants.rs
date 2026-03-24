b07a840e869d8f16e9e55e4f30cc3492a31bf4b6
cs01 <cs01@users.noreply.github.com>
2026-03-23 21:09:10 -0700

    bump subprotocol to v4, restrict signing key perms, add reconnect jitter

--- termpair-rs/src/constants.rs
+++ termpair-rs/src/constants.rs
@@ -1,5 +1,5 @@
 pub const TERMPAIR_VERSION: &str = "1.0.0";
-pub const SUBPROTOCOL_VERSION: &str = "3";
+pub const SUBPROTOCOL_VERSION: &str = "4";
 pub const MAX_READ_BYTES: usize = 2048;
 pub const IV_LENGTH: usize = 12;
 pub const KEY_LENGTH_BYTES: usize = 16;
